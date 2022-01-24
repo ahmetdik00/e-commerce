d3756b9b5bd66b7d7a0cc8008fb5aa0885f7514b
ahmet <ahmet199965@gmail.com>
2022-01-24 22:54:41 +0300

    First

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">Eticaret Yazılımı &copy; 2021</div>
-            <div class="col-md-6 text-right"><a href="http://www.uzaktankurs.com">Uzaktan Kurs</a></div>
+            <div class="col-md-6 text-right"><a target="_blank" href="https://www.linkedin.com/in/ahmet-dik-1325b9176/">Ahmet Dik</a></div>
         </div>
     </div>
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\AnasayfaController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\UrunController;
+use App\Http\Controllers\SepetController;
+use App\Http\Controllers\OdemeController;
+use App\Http\Controllers\SiparisController;
+use App\Http\Controllers\KullaniciController;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +19,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [AnasayfaController::class, 'index'])->name('Anasayfa');
+Route::get('/', [AnasayfaController::class, 'index'])->name('anasayfa');
 
-Route::view('/kategori', 'kategori');
-Route::view('/urun', 'urun');
-Route::view('/sepet', 'sepet');
+Route::get('/kategori/{slug_kategoriadi}', [KategoriController::class, 'index'])->name('kategori');
+
+Route::get('/urun/{slug_urunadi}', [UrunController::class, 'index'])->name('urun');
+
+Route::get('/sepet', [SepetController::class, 'index'])->name('sepet');
+
+Route::get('/odeme', [OdemeController::class, 'index'])->name('odeme');
+
+Route::get('/siparisler', [SiparisController::class, 'index'])->name('siparisler');
+
+Route::get('/siparisler/{id}', [SiparisController::class, 'detay'])->name('siparis');
+
+Route::prefix('kullanici')->group(function () {
+    Route::get('/oturumac', [KullaniciController::class, 'giris_form'])->name('kullanici.oturumac');
+    Route::get('/kaydol', [KullaniciController::class, 'kaydol_form'])->name('kullanici.kaydol');
+});
